584822ba27eba263a5abda04c9325866fc948a39
Gustavo Borges de Camargo <gustavo.camargo@uds.com.br>
2018-11-23 17:13:55 -0200

    estrutura inicial 3

---
+++ app/User.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Uuids;
+
+class User extends Authenticatable
+{
+    use Uuid, Notifiable;
+
+    public $incrementing = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'cpf',
+        'telephone',
+        'situation',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
