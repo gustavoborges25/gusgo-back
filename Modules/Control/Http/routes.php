efa51022c122061a1c297bef9b5b7d79e9ca86b7
Gustavo Borges de Camargo <gustavo.camargo@uds.com.br>
2019-04-15 12:35:32 -0300

    #5 - Alterado prefix das rota e nomeclatura ingles.

--- Modules/Control/Http/routes.php
+++ Modules/Control/Http/routes.php
@@ -1,20 +1,19 @@
 <?php
 
 // Authentication.
-Route::post('/auth', 'AuthController@authenticate');
-Route::post('/recover', 'AuthController@recoverPassword');
-Route::post('/register', 'AuthController@register');
+Route::group([
+    'prefix' => 'api',
+], function () {
+    // Authentication.
+    Route::post('/auth', 'AuthController@authenticate');
+    Route::post('/recover', 'AuthController@recoverPassword');
 
-Route::group(
-    [
-        'middleware' =>
-            [
-                'jwt.auth',
-            ],
-    ], function ()
-        {   // User methods
-            Route::get('/get-logged-user', 'UserController@getLoggedUser');
-            Route::get('/get-states-external', 'StateController@getStatesExternal');
-            Route::get('/get-cities-external/{state_id}', 'CityController@getCitiesExternal');
-        }
-);
+    Route::group([
+        'middleware' => ['jwt.auth'],
+    ], function () {
+        Route::get('/get-logged-user', 'UserController@getLoggedUser');
+        Route::get('/get-states-external', 'StateController@getStatesExternal');
+        Route::get('/get-cities-external/{state_id}', 'CityController@getCitiesExternal');
+
+    });
+});
